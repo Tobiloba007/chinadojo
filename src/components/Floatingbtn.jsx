107a23eccb6303b623dc4acd2c1373034bebcd53
Olaniyan michael <olaniyanmichael0@gmail.com>
2023-05-31 20:01:36 +0100

    changed whatsapp link

--- src/components/Floatingbtn.jsx
+++ src/components/Floatingbtn.jsx
@@ -4,7 +4,7 @@ import { IoLogoWhatsapp } from 'react-icons/io'
 const Floatingbtn = () => {
   return (
     <div className='fixed bottom-[40px] right-[30px] flex flex-col items-center justify-center md:bottom-[50px] md:right-[50px]'>
-    <a href='https://wa.me/message/DKLMJG655GNBM1' target="_blank">
+    <a href='https://api.whatsapp.com/send?phone=2348142154108&text=Hello%20ChinaDojo' target="_blank">
       <IoLogoWhatsapp className='text-5xl text-[#33a05d] md:text-6xl lg:text-7xl xl:text-8xl' />
     </a>
     <p className='text-xs font-[600] md:text-sm xl:text-lg'>
