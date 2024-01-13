f98cbcd60ff198edd7c6749bcd5fa2d0834c76b3
Michael <Olaniyanmichael0@gmail.com>
2024-01-13 08:11:08 +0100

    fix2

--- sanity.js
+++ sanity.js
@@ -6,7 +6,6 @@ const client = createClient({
   dataset: 'production',
   useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: '2022-03-07', // use current date (YYYY-MM-DD) to target the latest API version
-  apiHost: 'https://chinadojo.sanity.studio',
 })
 
 export default client
